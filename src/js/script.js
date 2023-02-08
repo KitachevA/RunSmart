3f1b6e3104c2c149708221a52169e4410f76085e
Artem <periphery290@gmail.com>
2023-02-08 14:47:10 +0200

    implemented tabs by Jquery + section(feedback)

--- src/js/script.js
+++ src/js/script.js
@@ -25,3 +25,33 @@ document.querySelector('.carousel__inner-prev').addEventListener('click', functi
 document.querySelector('.carousel__inner-next').addEventListener('click', function(){
     slider.goTo('next');
 });
+
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+    })(jQuery);
+
+
+    const btnBack = document.querySelectorAll('.catalog-item__back'),
+          more = document.querySelectorAll('.catalog-item__link');
+
+   function toggleSlide(tabs){
+    tabs.forEach((btn, i)=>{
+        btn.addEventListener('click', (e)=>{
+            e.preventDefault();
+            document.querySelectorAll('.catalog-item__content')[i].classList.toggle('catalog-item__content_active');
+            document.querySelectorAll('.catalog-item__list')[i].classList.toggle('catalog-item__list_active');
+         
+        });
+    });
+   }
+
+   toggleSlide(btnBack);
+   toggleSlide(more);
